ccd95b6440cb60ec6a00890b91d1bc84eab1e54a
BabyRexPlayz <86994665+BabyRexPlayz@users.noreply.github.com>
2021-11-09 07:51:35 +0800

    Add files via upload

---
+++ src/ssid.py
@@ -0,0 +1,148 @@
+from bot import send_webhook, embed_from_group, get_group_funds
+import socket
+import ssl
+import json
+import logging
+import traceback
+logging.getLogger().setLevel(logging.CRITICAL)
+
+class ResponseError(Exception):
+    pass
+
+def scanner_func(
+        worker_num, thread_num,
+        thread_barrier, thread_event,
+        proxies, timeout, no_close,
+        webhook_url,
+        local_counter,
+        gid_counter, gid_range, gid_ignore, gid_cutoff,
+        min_funds, min_members
+    ):
+    gid = None
+    ssl_context = ssl.create_default_context()
+    thread_barrier.wait()
+    thread_event.wait()
+
+    while True:
+        proxy_addr = None
+        if proxies:
+            proxy_addr = next(proxies)
+
+        # establish connection to groups.roblox.com
+        sock = None
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(5)
+            if proxy_addr:
+                sock.connect(proxy_addr)
+                sock.send(b"CONNECT groups.roblox.com:443 HTTP/1.1\r\n\r\n")
+                sock.recv(1024**2)
+            else:
+                sock.connect(("groups.roblox.com", 443))
+            sock = ssl_context.wrap_socket(sock, server_hostname="groups.roblox.com")
+            
+        except Exception as err:
+            logging.warning(
+                f"Couldn't establish connection to groups.roblox.com (proxy {proxy_addr}): {err!r}")
+            if sock:
+                try:
+                    sock.shutdown(socket.SHUT_RDWR)
+                except OSError:
+                    pass
+                sock.close()
+            continue
+        
+        # scan for claimable groups matching criteria
+        while True:
+            if gid is None:
+                gid = gid_range[0] + next(gid_counter) % (gid_range[1] - gid_range[0])
+                
+            # skip previously ignored groups
+            if gid in gid_ignore:
+                gid = None
+                continue
+            
+            try:
+                # send request and read response
+                sock.send(f"GET /v1/groups/{gid} HTTP/1.1\r\nHost: groups.roblox.com\r\n\r\n".encode())
+                resp = sock.recv(1024**2)
+
+                # ratelimited / ip blocked
+                if resp.startswith(b"HTTP/1.1 429") or resp.startswith(b"HTTP/1.1 403"):
+                    raise ResponseError(
+                        "Ratelimit or IP is blocked")
+                # invalid group
+                if resp.startswith(b"HTTP/1.1 400") and b"Group is invalid or does not exist." in resp:
+                    if not gid_cutoff or gid_cutoff >= gid:
+                        gid_ignore[gid] = True
+                    local_counter.count()
+                    gid = None
+                    continue
+                # unexpected status
+                if not resp.startswith(b"HTTP/1.1 200"):
+                    raise ResponseError(
+                        f"Unrecognized statusline while reading response: {resp[:20]}")
+                
+                data = json.loads(resp.split(b"\r\n\r\n", 1)[1])
+                local_counter.count()
+
+                # skip and ignore locked groups
+                if data.get("isLocked"):
+                    gid_ignore[gid] = True
+                    gid = None
+                    continue
+                # skip and ignore no owner & no public entry groups
+                if not data.get("owner") and not data.get("publicEntryAllowed"):
+                    gid_ignore[gid] = True
+                    gid = None
+                    continue
+                # skip unclaimable groups
+                if data.get("owner") or not data.get("publicEntryAllowed"):
+                    gid = None
+                    continue
+                # skip groups with less members than specified
+                if min_members and min_members > data["memberCount"]:
+                    gid = None
+                    continue
+                
+                # get amount of group funds
+                funds = None
+                for _ in range(3):
+                    try:
+                        funds = get_group_funds(
+                            gid,
+                            proxy_addr=proxies and next(proxies),
+                            timeout=timeout)
+                        break
+                    except:
+                        pass
+
+                # skip groups with less funds than specified
+                if min_funds and (not funds or min_funds > funds):
+                    gid = None
+                    continue
+
+                # avoid notifying user about the same group multiple times
+                gid_ignore[gid] = True
+                
+                # log group info to console (id - name - members - funds)
+                print(f"\rFound group: {data['id']} - {data['name']} - {data['memberCount']} - {f'{funds} R$' if funds is not None else '?'}" + (" " * 30), end="\n")
+                
+                if webhook_url:
+                    # send group details to webhook
+                    send_webhook(webhook_url, embeds=[embed_from_group(data, funds)])
+
+                gid = None
+
+            except Exception as err:
+                logging.warning(f"Dropping connection due to error: {err!r}")
+                if isinstance(err, ResponseError) and no_close:
+                    continue
+                break
+        
+        try:
+            sock.shutdown(socket.SHUT_RDWR)
+        except OSError:
+            pass
+        sock.close()
+        
---
+++ src/workers.py
@@ -0,0 +1,64 @@
+from ssid import scanner_func
+from thread import ChunkCounter
+import multiprocessing
+import threading
+import itertools
+import os
+def set_cpu_affinity(cpu_num):
+    if os_name == "nt":
+        from .batch import set_cpu_affinity
+        return set_cpu_affinity(0, 1 << cpu_num)
+    else: pass
+
+def worker_func(worker_num, worker_barrier, thread_count,
+                count_queue,
+                webhook_url,
+                proxies, timeout, no_close,
+                gid_range, gid_cutoff,
+                min_funds, min_members):
+    # set cpu affinity for this process
+
+    proxies = proxies and itertools.cycle(proxies) or None
+    local_counter = ChunkCounter(notify_per=1000)
+    gid_counter = itertools.count(0)
+    gid_ignore = {}
+
+    # create & start threads
+    thread_barrier = threading.Barrier(thread_count + 1)
+    thread_event = threading.Event()
+    threads = [
+        threading.Thread(
+            target=scanner_func,
+            kwargs=dict(
+                worker_num=worker_num,
+                thread_num=thread_num,
+                thread_barrier=thread_barrier,
+                thread_event=thread_event,
+                webhook_url=webhook_url,
+                proxies=proxies,
+                timeout=timeout,
+                no_close=no_close,
+                local_counter=local_counter,
+                gid_counter=gid_counter,
+                gid_range=gid_range,
+                gid_ignore=gid_ignore,
+                gid_cutoff=gid_cutoff,
+                min_funds=min_funds,
+                min_members=min_members
+            )
+        )
+        for thread_num in range(thread_count)
+    ]
+
+    for thread in threads:
+        thread.start()
+    # wait for threads to initialize
+    thread_barrier.wait()
+    # wait for other workers
+    worker_barrier.wait()
+    # notify threads
+    thread_event.set()
+    
+    # pass counts to main process
+    while any(t.is_alive() for t in threads):
+        count_queue.put(local_counter.wait())
